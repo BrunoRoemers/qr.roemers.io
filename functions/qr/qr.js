99e79c31c19f65ba783161a846db3b66f6c32658
Bruno Roemers <bruno@roemers.io>
2020-08-21 20:57:41 +0200

    Redirect based on qr details data

--- functions/qr/qr.js
+++ functions/qr/qr.js
@@ -1,32 +1,52 @@
-const locs = {
-  'test': 'https://roemers.io',
-}
+const aws = require('aws-sdk')
+const awsConfig = require('../../helpers/aws-config')
+const UrlPattern = require('url-pattern')
 
-// Entry point for /id/*
-exports.handler = (event, context, callback) => {
-  // sanitize id
-  const id = encodeURIComponent(event.queryStringParameters.id)
+const endpoint = new UrlPattern(process.env['QR_ENDPOINT'])
 
-  console.log('=== EVENT')
-  console.log(event)
-  console.log('=== CONTEXT')
-  console.log(context)
+// Fallback redirect
+const fallback = (ref, cb) => cb(null, {
+  statusCode: 301,
+  headers: {
+    location: `https://roemers.io?ref=${ref}`,
+  },
+})
 
-  // fallback redirect
-  if (!locs.hasOwnProperty(id)) {
-    return callback(null, {
-      statusCode: 301,
-      headers: {
-        location: 'https://roemers.io?ref=qr-fallback',
-      }
-    })
+exports.handler = async (event, ctx, cb) => {
+  const frags = endpoint.match(event.path)
+
+  // uuid not provided
+  if (!frags || !frags.uuid) return fallback('qr-no-id', cb)
+
+  // sanitize uuid
+  const uuid = encodeURIComponent(frags.uuid)
+
+  // setup connection
+  const db = new aws.DynamoDB.DocumentClient(awsConfig())
+
+  // get qr details
+  const params = {
+    TableName: process.env['QR_TABLE_DETAILS'],
+    Key: { 'qr-uuid': uuid, },
+    ReturnConsumedCapacity: 'TOTAL',
   }
+  const res = await db.get(params).promise()
+  const cu = res.ConsumedCapacity.CapacityUnits
+  const details = res.Item
 
-  // redirect
-  callback(null, {
+  // uuid not found in db
+  if (!details || !details['qr-location']) fallback('qr-no-details', cb)
+  
+  // redirect to final destination
+  cb(null, {
     statusCode: 301,
     headers: {
-      location: locs[id],
+      location: encodeURI(details['qr-location'])
     }
   })
+
+  // console.log('=== EVENT')
+  // console.log(event)
+  // console.log('=== CONTEXT')
+  // console.log(ctx)
 }
